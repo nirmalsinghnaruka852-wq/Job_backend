685ed5dbb597b3373c8b48df992b6745788f8e73
Nirmal Singh Naruaka <nirmalsinghnaruka852@gmail.com>
2026-01-09 21:11:24 +0530

    init the standart repo structure of the background

--- src/server.ts
+++ src/server.ts
@@ -1,1 +1,25 @@
-    
+ import express, { Request, Response, NextFunction } from "express";
+import cors from "cors";
+import dotenv from "dotenv";
+
+dotenv.config();
+
+const app = express();
+const PORT = process.env.PORT || 5000;
+
+
+app.use(cors());
+app.use(express.json());
+
+app.get("/", (req: Request, res: Response) => {
+  res.send("Job Finder Backend is running ✅");
+});
+
+app.use((err: any, req: Request, res: Response, next: NextFunction) => {
+  console.error(err.stack);
+  res.status(500).json({ message: "Something went wrong!" });
+});
+
+app.listen(PORT, () => {
+  console.log(`Server running on http://localhost:${PORT}`);
+});
